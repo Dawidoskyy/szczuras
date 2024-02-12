9879da2fc85fd5a81e487c056f6e3291875a12b4
ventu <67205165+gsventu@users.noreply.github.com>
2024-02-12 20:06:54 +0100

    cookisy

--- auth.php
+++ auth.php
@@ -1,62 +1,79 @@
 <?php
-    session_start();
+session_start();
 
-    if($_SESSION['lastaction'] + 3 > time()) {
+if(isset($_SESSION['authkey'])) {
+    $_SESSION['lastaction'] = time();
+
+    header('Location: index.php');
+    exit();
+}
+
+if(isset($_COOKIE['login_key'])) {
+    $login_key = $_COOKIE['login_key'];
+
+    require "inc/sql_connect.php";
+    require "inc/sql_funcs.php";
+
+    $sql = "SELECT * FROM users WHERE authkey = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $login_key);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+
+        $_SESSION['authkey'] = $login_key;
+        $_SESSION['username'] = $row['username'];
+        $_SESSION['subscription'] = $row['subscription'];
+        $_SESSION['lookups'] = $row['lookups'];
+        $_SESSION['admin'] = $row['admin'];
+
+        $_SESSION['lastaction'] = time();
+
+        header('Location: index.php');
+        exit();
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login_key'])) {
+    $login_key = $_POST['login_key'];
+
+    require "inc/sql_connect.php";
+    require "inc/sql_funcs.php";
+
+    $sql = "SELECT * FROM users WHERE authkey = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $login_key);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+
+        $_SESSION['authkey'] = $login_key;
+        $_SESSION['username'] = $row['username'];
+        $_SESSION['subscription'] = $row['subscription'];
+        $_SESSION['lookups'] = $row['lookups'];
+        $_SESSION['admin'] = $row['admin'];
+
+        setcookie("login_key", $login_key, time() + 3600, "/");
+
+        $_SESSION['lastaction'] = time();
+
+        header('Location: index.php');
+        exit();
+    } else {
         $_SESSION['error_style'] = 0;
-        $_SESSION['error_message'] = "Too many requests to api. Try again later.";
+        $_SESSION['error_message'] = "Invalid key.";
+
+        unset($_SESSION['authkey']);
+        
         header('Location: index.php');
         exit();
     }
 
-    $_SESSION['lastaction'] = time();
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login_key'])) {
-        $login_key = $_POST['login_key'];
-
-        require "inc/sql_connect.php";
-        require "inc/sql_funcs.php";
-
-        $sql = "SELECT * FROM users WHERE authkey = ?";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("s", $login_key);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        if ($result->num_rows > 0) {
-            $row = $result->fetch_assoc();
-
-            $_SESSION['authkey'] = $login_key;
-
-            $_SESSION['username'] = $row['username'];
-            $_SESSION['subscription'] = $row['subscription'];
-            $_SESSION['lookups'] = $row['lookups'];
-            $_SESSION['admin'] = $row['admin'];
-
-            $_SESSION['error_style'] = 1;
-            $_SESSION['error_message'] = "Successfully logged in.";
-
-            // Logs
-            $logsData = [
-                'user' => $row['username'],
-                'authkey' => $login_key,
-                'IP' => $_SERVER['REMOTE_ADDR'],
-                'date' => time()
-            ];
-            addNewRecord($conn, 'login_logs', $logsData);
-
-            header('Location: index.php');
-            exit();
-        } else {
-            $_SESSION['error_style'] = 0;
-            $_SESSION['error_message'] = "Invalid key.";
-
-            unset($_SESSION['authkey']);
-            
-            header('Location: index.php');
-            exit();
-        }
-
-        $stmt->close();
-        $conn->close();
-    }
+    $stmt->close();
+    $conn->close();
+}
 ?>
