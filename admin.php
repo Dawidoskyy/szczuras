87f3016f2f07df302f3592dec8ffb7b94cf97e6f
ventu <67205165+gsventu@users.noreply.github.com>
2024-02-12 20:49:15 +0100

    title

--- admin.php
+++ admin.php
@@ -15,7 +15,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.css">
     <link rel="stylesheet" href="styles/style.css">
-    <title>szczuras.fun</title>
+    <title>gicik.xyz</title>
     <style>
         .hidden { display: none; }
     </style>
