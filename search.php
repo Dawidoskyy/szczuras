c3ab0aca1b4cfeafe32cb60dba5e4ef45a429e24
ventu <67205165+gsventu@users.noreply.github.com>
2024-03-20 00:22:26 +0100

    wyszukiwanie po non-premce

--- search.php
+++ search.php
@@ -42,7 +42,54 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['search_user'])) {
     $mojang_response = file_get_contents($mojang_api_url);
 
     if ($mojang_response === false) {
-        search_api_error("Error retrieving data from API #1.", $search_key);
+        $search_results = [];
+    
+        $sql = "SELECT * FROM ipki WHERE nickname = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $search_key);
+        $stmt->execute();
+        $result = $stmt->get_result();
+    
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $search_results[] = $row;
+            }
+        }
+    
+        if (!empty($search_results)) {
+            $_SESSION['modal_message'] = "<center><h1 class='jebanelogo' style='font-size: 35px;'>User found!</h1><br>
+            <table>
+                <tr>
+                    <th>Username</th>
+                    <th>IP</th>
+                    <th>From</th>
+                    <th></th>
+                </tr>";
+    
+            foreach ($search_results as $row) {
+                $_SESSION['modal_message'] .= "<tr>
+                        <td>" . $row['nickname'] . "</td>
+                        <td>" . $row['ip'] . "</td>
+                        <td>" . $row['fromwhere'] . "</td>
+                        <td><button class='chujowyprzycisk' onclick=\"var textarea = document.createElement('textarea');textarea.value = '" . $row['ip'] . "';document.body.appendChild(textarea);textarea.select();document.execCommand('copy');document.body.removeChild(textarea);\">Copy IP</button></td>
+                    </tr>";
+            }
+    
+            $_SESSION['lookups'] += count($search_results);
+            updateRecord($conn, 'users', 'lookups', $_SESSION['lookups'], 'authkey', $_SESSION['authkey']);
+    
+            $logsData = [
+                'user' => $_SESSION['username'],
+                'value' => $search_key,
+                'status' => 1
+            ];
+            addNewRecord($conn, 'search_logs', $logsData);
+        } else {
+            search_api_error("User not found!", $search_key);
+        }
+    
+        header('Location: index.php');
+        exit();
     }
 
     $mojang_data = json_decode($mojang_response, true);
